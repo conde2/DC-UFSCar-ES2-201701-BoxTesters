601f70a94b91b3ad427b30d274a142c0ef1fc523
matheusDeAlmeida <mat.sts.amd@gmail.com>
2017-07-21 00:59:25 -0300

    CSV
    foram adicionados valores de retorno para a classe csvImporter para que o projeto possa ser testado

--- src/main/java/org/jabref/logic/importer/fileformat/CSVImporter.java
+++ src/main/java/org/jabref/logic/importer/fileformat/CSVImporter.java
@@ -2,11 +2,19 @@ package org.jabref.logic.importer.fileformat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 import org.jabref.logic.importer.Importer;
 import org.jabref.logic.importer.ParserResult;
 import org.jabref.logic.util.FileExtensions;
-
-
+import org.jabref.model.entry.AuthorList;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.FieldName;
+import org.jabref.model.entry.BibtexEntryTypes;
 
 public class CSVImporter extends Importer {
 
@@ -27,10 +35,16 @@ public class CSVImporter extends Importer {
 
     @Override
     public boolean isRecognizedFormat(BufferedReader reader) throws IOException {
+        String buffer;
+
+		return false;
     }
 
     @Override //ver OvidImporter.java, BibTeXMLImporter.java
     public ParserResult importDatabase(BufferedReader input) throws IOException {
+        List<BibEntry> bibitems = new ArrayList<>();
+		
+        return new ParserResult(bibitems);
     }
 
 
